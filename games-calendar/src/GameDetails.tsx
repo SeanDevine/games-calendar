63446ae7d710767e3276fdb90e6d462be8fe943d
SeanDevine <sean_devine@hotmail.ca>
2024-03-18 13:53:04 -0700

    Add flexbox to game details page

--- games-calendar/src/GameDetails.tsx
+++ games-calendar/src/GameDetails.tsx
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
+import { Container, Header, Content, Footer, Sidebar, FlexboxGrid } from 'rsuite';
+import FlexboxGridItem from 'rsuite/esm/FlexboxGrid/FlexboxGridItem';
 
 const apiKey = 'c9bf349e90a04c5f852186b91ab54688';
 const apiUrl = 'https://api.rawg.io/api/';
@@ -28,26 +30,26 @@ const GameDetails = () => {
   }, [id]);
 
   return (
-    <div>
-      {gameDetails && (
-        <div>
-          <div style={{ display: 'flex' }}>
-            <div style={{ flex: '1 0 40%', paddingRight: '20px' }}>
-              <img src={gameDetails.background_image} alt={gameDetails.name} style={{ width: '100%', maxHeight: '300px', objectFit: 'cover' }} />
-            </div>
-            <div style={{ flex: '1 0 60%' }}>
-              <h2 className='gameTitle'>{gameDetails.name}</h2>
-              <p className='labelText'><strong className='labelName'>Release Date:</strong> {gameDetails.released}</p>
-              <p className='labelText'><strong className='labelName'>Platforms:</strong> {gameDetails.platforms.map((platform: any) => platform.platform.name).join(', ')}</p>
-            </div>
-          </div>
+    <FlexboxGrid justify='center'>
+      <FlexboxGridItem colspan={18}>
+        {gameDetails && (
           <div>
-            <h6 className='labelText'>Description</h6>
-            <p>{gameDetails.description_raw}</p>
+            <div style={{ display: 'flex' }}>
+              <div style={{ flex: '1 0 40%', paddingRight: '20px' }}>
+                <img className='gameImage' src={gameDetails.background_image} alt={gameDetails.name}/>
+              </div>
+              <div style={{ flex: '1 0 60%' }}>
+                <h2 className='gameTitle'>{gameDetails.name}</h2>
+                <p className='labelText'><strong className='labelName'>Release Date:</strong> {gameDetails.released}</p>
+                <p className='labelText'><strong className='labelName'>Platforms:</strong> {gameDetails.platforms.map((platform: any) => platform.platform.name).join(', ')}</p>
+                <p className='labelText labelName'>Description:</p>
+                <p>{gameDetails.description_raw}</p>
+              </div>
+            </div>
           </div>
-        </div>
-      )}
-    </div>
+        )}
+      </FlexboxGridItem>
+    </FlexboxGrid>
   );
 };
 
